a43ddddfdb86c6b57aef0b8741f1b0ecbd31d9d5
Igor Alekseev <i.alekseev@iac.spb.ru>
2021-11-26 11:44:52 +0300

    Start

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { View, ScreenSpinner, AdaptivityProvider, AppRoot } from '@vkontakte/vku
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
-import Persik from './panels/Persik';
 
 const App = () => {
     const [activePanel, setActivePanel] = useState('home');
@@ -27,16 +26,11 @@ const App = () => {
         fetchData();
     }, []);
 
-    const go = e => {
-        setActivePanel(e.currentTarget.dataset.to);
-    };
-
     return (
         <AdaptivityProvider>
             <AppRoot>
                 <View activePanel={activePanel} popout={popout}>
-                    <Home id='home' fetchedUser={fetchedUser} go={go} />
-                    <Persik id='persik' go={go} />
+                    <Home id='home' fetchedUser={fetchedUser}/>
                 </View>
             </AppRoot>
         </AdaptivityProvider>
