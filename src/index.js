8a8031802fcae8fb2524799fc663b60a6ba95ec7
Oleshchuk Andrew <a.oleshchuk@iac.spb.ru>
2021-11-26 14:42:32 +0300

    stage 2

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import App from "./App";
 bridge.send("VKWebAppInit");
 
 ReactDOM.render(<App />, document.getElementById("root"));
-if (process.env.NODE_ENV === "development") {
-  import("./eruda").then(({ default: eruda }) => {}); //runtime download
-}
+// if (process.env.NODE_ENV === "development") {
+//   import("./eruda").then(({ default: eruda }) => {}); //runtime download
+// }
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
-import {Panel, PanelHeader} from '@vkontakte/vkui';
+import { Group, Header, HorizontalCell, HorizontalScroll, Panel, PanelHeader, Search} from '@vkontakte/vkui';
 import {useCustomState} from "../context/context";
 import {StartupVariable} from "../startup-variable";
 import bridge from "@vkontakte/vk-bridge";
@@ -9,14 +9,57 @@ import bridge from "@vkontakte/vk-bridge";
 
 const Home = ({id}) => {
     const {state: globalState, dispatch} = useCustomState()
+    const [bookList, setbookList] = useState(null);
+    const [search, setSearch] = useState('');
+
+    async function bookListfn() {
+        const list = await fetch(process.env.REACT_APP_API_URL + '/catalog/latest?page=1&size=10').then(resp => resp.json())
+        await setbookList(list);
+    }
+
     useEffect(() => {
         bridge.send("VKWebAppGetUserInfo").then((e) => dispatch({type: 'init', payload: {...e, ...StartupVariable}}));
+        bookListfn()
     }, [])
 
+
+    function onClick (e) {
+        setSearch(e.target.value)
+        console.log('search', search);
+    }
     return (
-        <Panel id={id}>
-            {globalState.state &&  <PanelHeader> Hello {globalState.state.last_name}  your app_id {globalState.state.app_id}</PanelHeader>}
-        </Panel>
+        <>
+            <Panel id={id}>
+                {globalState.state &&
+                <PanelHeader> Hello {globalState.state.last_name} your app_id {globalState.state.app_id}</PanelHeader>}
+                {
+                    bookList &&
+                    <Group header={<Header>Список книг</Header>}>
+                        <HorizontalScroll showArrows getScrollToLeft={i => i - 60} getScrollToRight={i => i + 60}>
+                            <div style={{display: 'flex'}}>
+                                {bookList.map((book) => {
+                                        return (
+                                            <HorizontalCell key={book.smallCover} size='l' header={book.title} >
+                                                <img src={book.smallCover} alt={'smallCover'}/>
+                                            </HorizontalCell>
+                                        )
+                                    }
+                                )}
+                            </div>
+                        </HorizontalScroll>
+                    </Group>
+                }
+                {
+                    <Group>
+                        <Search value={search} onChange={(e) =>onClick(e)} after={null}/>
+                    </Group>
+                }
+            </Panel>
+
+
+        </>
+
+
     );
 }
 
