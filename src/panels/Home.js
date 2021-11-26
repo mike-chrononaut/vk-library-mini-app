af52b6162e2f839291aeda39540f6016e49bf80e
sil sosil <sil@sosil.ru>
2021-11-26 13:23:06 +0300

    book card list

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,18 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
+import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar, CardScroll, Card, FormItem, Input } from '@vkontakte/vkui';
 import AppContext from '../AppContext';
 
 const Home = ({ id }) => {
-
 	const {userInfo, launchParams} = useContext(AppContext)
+	const [page, setPage] = useState(1)
+	const [books, setBooks] = useState([])
 
+	useEffect(() => {
+		async function loadBookList() {
+			const url = new URL(`${process.env.REACT_APP_API_URL}/catalog/latest`);
+			const params = {page: 1}
+				Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+			
+			const resp = await fetch(url)
+			const books = await resp.json()
+			setBooks(books)
+		}
 
+		loadBookList()
+	}, [])
+
+	const onSearchChange = (event) => {
+		console.log(event.target.value)
+	}
 
 	return (
 	<Panel>
-		<PanelHeader>{ userInfo && launchParams && <span>Привет, {userInfo.first_name} ( {launchParams.vk_app_id} ) </span>}</PanelHeader>
+		<PanelHeader>{ userInfo && launchParams && <span>Привет, {userInfo.first_name}</span>}</PanelHeader>
+		<Group description="Последние книги">
+			<CardScroll size="s">
+				{books && books.map(b => { return(
+					<Card style={{width: '150px'}} key={b.id}>
+						<div style={{ paddingBottom: '200px', background: `url(${b.smallCover})`, objectFit: 'cover', backgroundSize: '100%', backgroundRepeat: 'no-repeat' }} />
+					</Card>
+				)})}
+			</CardScroll>
+			<FormItem>
+				<Input onChange={onSearchChange} type="text" placeholder='Поиск' defaultValue="" align="center" />
+			</FormItem>
+		</Group>
 		{/* 
 		{fetchedUser &&
 		<Group header={<Header mode="secondary">Пользователь</Header>}>
