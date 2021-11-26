1927e42e39432c90faf02d60777a65fdaf9b35a3
m.yakovlev <m.yakovlev@iac.spb.ru>
2021-11-26 06:19:40 +0300

    example for initial stage

--- src/App.js
+++ src/App.js
@@ -1,48 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import {AdaptivityProvider, AppRoot, ScreenSpinner, View} from '@vkontakte/vkui';
+import { View, ScreenSpinner, AdaptivityProvider, AppRoot } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
 import Persik from './panels/Persik';
-import AppContext from "./AppContext";
 
 const App = () => {
     const [activePanel, setActivePanel] = useState('home');
     const [fetchedUser, setUser] = useState(null);
-    const [popout, setPopout] = useState(<ScreenSpinner size='large'/>);
-
-    const [context, setContext] = useState({});
+    const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
 
     useEffect(() => {
-        bridge.subscribe(({detail: {type, data}}) => {
+        bridge.subscribe(({ detail: { type, data }}) => {
             if (type === 'VKWebAppUpdateConfig') {
                 const schemeAttribute = document.createAttribute('scheme');
                 schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
                 document.body.attributes.setNamedItem(schemeAttribute);
             }
         });
-
-        async function fetchUserInfo() {
+        async function fetchData() {
             const user = await bridge.send('VKWebAppGetUserInfo');
             setUser(user);
             setPopout(null);
-            return user;
         }
-
-        async function parseSearchParams() {
-            const urlSearchParams = new URLSearchParams(window.location.search);
-            return Object.fromEntries(urlSearchParams.entries());
-        }
-
-        async function createContext() {
-            let userInfo = await fetchUserInfo();
-            let searchParams = await parseSearchParams();
-
-            setContext({"userInfo": userInfo, "searchParams": searchParams});
-        }
-
-        createContext();
+        fetchData();
     }, []);
 
     const go = e => {
@@ -52,12 +34,10 @@ const App = () => {
     return (
         <AdaptivityProvider>
             <AppRoot>
-                <AppContext.Provider value={[context, setContext]}>
-                    <View activePanel={activePanel} popout={popout}>
-                        <Home id='home' fetchedUser={fetchedUser} go={go}/>
-                        <Persik id='persik' go={go}/>
-                    </View>
-                </AppContext.Provider>
+                <View activePanel={activePanel} popout={popout}>
+                    <Home id='home' fetchedUser={fetchedUser} go={go} />
+                    <Persik id='persik' go={go} />
+                </View>
             </AppRoot>
         </AdaptivityProvider>
     );
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,15 +1,42 @@
-import React, {useContext} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import {Panel, PanelHeader} from '@vkontakte/vkui';
-import AppContext from "../AppContext";
+import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
 
-const Home = ({id, go}) => {
-    const [context, setContext] = useContext(AppContext);
+const Home = ({ id, go, fetchedUser }) => (
+    <Panel id={id}>
+        <PanelHeader>Example</PanelHeader>
+        {fetchedUser &&
+        <Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
+            <Cell
+                before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+                description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+            >
+                {`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+            </Cell>
+        </Group>}
 
-    return (<Panel id={id}>
-        {context.userInfo &&
-        <PanelHeader style={{fontWeight: "bold"}}>Здравствуйте, {context.userInfo.first_name}!</PanelHeader>}
-    </Panel>);
-}
+        <Group header={<Header mode="secondary">Navigation Example</Header>}>
+            <Div>
+                <Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
+                    Show me the Persik, please
+                </Button>
+            </Div>
+        </Group>
+    </Panel>
+);
+
+Home.propTypes = {
+    id: PropTypes.string.isRequired,
+    go: PropTypes.func.isRequired,
+    fetchedUser: PropTypes.shape({
+        photo_200: PropTypes.string,
+        first_name: PropTypes.string,
+        last_name: PropTypes.string,
+        city: PropTypes.shape({
+            title: PropTypes.string,
+        }),
+    }),
+};
 
 export default Home;
